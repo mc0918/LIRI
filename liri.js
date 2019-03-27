32533ad48698454979d24ea492ed621bfb2868b4
mc0918 <mcampbell0918@gmail.com>
2019-03-26 20:33:13 -0500

    spotify results print to console

--- liri.js
+++ liri.js
@@ -18,11 +18,26 @@ const arg = process.argv;
 var userInput = arg.slice(2);
 console.log(userInput);
 
+if (process.argv.length === 2) {
+  userInput = "The Sign";
+}
+
 //Use user input to search spotify
 spotify
   .search({ type: "track", query: userInput })
   .then(function(response) {
-    console.log(response.tracks.items[0]);
+    console.log(response.tracks);
+    console.log("-----------");
+    console.log(
+      "Artist: " +
+        response.tracks.items[0].artists[0].name +
+        "\nSong: " +
+        response.tracks.items[0].name +
+        "\nPreview: " +
+        response.tracks.items[0].preview_url +
+        "\nAlbum: " +
+        response.tracks.items[0].album.name
+    );
   })
   .catch(function(err) {
     console.log(err);
